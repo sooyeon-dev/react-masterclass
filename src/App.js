ed5c7bab3e8b066d9d56a81eee571d1f85dbb1a9
sooyeon-dev <dus3062@daum.net>
2023-05-11 00:07:49 +0900

    styled component4
    - theme 이용
    - 다크모드 / 라이트모드 전환 시 사용 할 수 있음
    *대신 property 이름 동일

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor };
+`;
+
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const animation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  } 
-  50% {
-    transform: rotate(180deg);
-    border-radius: 50px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${animation} 1s linear infinite;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  ${Emoji}:hover { // styled component 타겟팅
-    font-size: 98px;
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor };
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😘</Emoji>
-      </Box>
+      <Title>제목</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111"
+}
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke"
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
